662b7159d61289e3fefe4316dd67342214f5ed9d
Carlo DiCelico <carlodicelico@gmail.com>
2014-05-14 21:20:33 -0400

    fix version number for NPM module

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
   summary: "Tokbox API for Meteor"
 });
 
-Npm.depends({"opentok" : "1.0.3"});
+Npm.depends({"opentok" : "1.0.2"});
 
 Package.on_use(function(api) {
   api.add_files(['server/index.js'], 'server');
